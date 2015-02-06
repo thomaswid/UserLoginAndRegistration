ce8768f98f8d9ef89e2e7d181b06efc132bea2ee
Widmarc Thomas <stylearama@gmail.com>
2015-02-06 13:10:38 -0500

    Initial Commit

---
+++ UserLoginAndRegistration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserLoginAndRegistration
+//
+//  Created by Morpheus Maximus on 2/6/15.
+//  Copyright (c) 2015 MyBln Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
